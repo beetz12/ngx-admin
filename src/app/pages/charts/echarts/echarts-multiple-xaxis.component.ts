d92edf737327475587946ab6d626d08083e8b79e
david <beetz12@gmail.com>
2024-07-21 17:04:51 -0400

    fixed file

--- src/app/pages/charts/echarts/echarts-multiple-xaxis.component.ts
+++ src/app/pages/charts/echarts/echarts-multiple-xaxis.component.ts
@@ -3,20 +3,16 @@ import { NbThemeService } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-echarts-multiple-xaxis',
-  template: `
-    <div echarts [options]="options" class="echart"></div>
-  `,
+  template: ` <div echarts [options]="options" class="echart"></div> `,
 })
 export class EchartsMultipleXaxisComponent implements AfterViewInit, OnDestroy {
   options: any = {};
   themeSubscription: any;
 
-  constructor(private theme: NbThemeService) {
-  }
+  constructor(private theme: NbThemeService) {}
 
   ngAfterViewInit() {
-    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
-
+    this.themeSubscription = this.theme.getJsTheme().subscribe((config) => {
       const colors: any = config.variables ? config.variables : {};
       const echarts: any = config.variables ? config.variables['echarts'] : {};
 
@@ -58,13 +54,13 @@ export class EchartsMultipleXaxisComponent implements AfterViewInit, OnDestroy {
             },
             axisPointer: {
               label: {
-<<<<<<< HEAD
-                formatter: params => {
-=======
                 formatter: (params: any) => {
->>>>>>> b47697a (fixed bootstrap import)
                   return (
-                    'Precipitation  ' + params.value + (params.seriesData.length ? '：' + params.seriesData[0].data : '')
+                    'Precipitation  ' +
+                    params.value +
+                    (params.seriesData.length
+                      ? '：' + params.seriesData[0].data
+                      : '')
                   );
                 },
               },
@@ -102,13 +98,13 @@ export class EchartsMultipleXaxisComponent implements AfterViewInit, OnDestroy {
             },
             axisPointer: {
               label: {
-<<<<<<< HEAD
-                formatter: params => {
-=======
                 formatter: (params: any) => {
->>>>>>> b47697a (fixed bootstrap import)
                   return (
-                    'Precipitation  ' + params.value + (params.seriesData.length ? '：' + params.seriesData[0].data : '')
+                    'Precipitation  ' +
+                    params.value +
+                    (params.seriesData.length
+                      ? '：' + params.seriesData[0].data
+                      : '')
                   );
                 },
               },
@@ -155,13 +151,19 @@ export class EchartsMultipleXaxisComponent implements AfterViewInit, OnDestroy {
             type: 'line',
             xAxisIndex: 1,
             smooth: true,
-            data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+            data: [
+              2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0,
+              2.3,
+            ],
           },
           {
             name: '2016 Precipitation',
             type: 'line',
             smooth: true,
-            data: [3.9, 5.9, 11.1, 18.7, 48.3, 69.2, 231.6, 46.6, 55.4, 18.4, 10.3, 0.7],
+            data: [
+              3.9, 5.9, 11.1, 18.7, 48.3, 69.2, 231.6, 46.6, 55.4, 18.4, 10.3,
+              0.7,
+            ],
           },
         ],
       };
